14c887205eedccf842b36f7f3b4463d0ada5d21f
Hopie <n.ngoc.dang.quang@moneyforward.co.jp>
2024-09-24 08:55:38 +0700

    Warmup game for lifestyle topic in MFV English huddle club.

---
+++ scripts.js
@@ -0,0 +1,79 @@
+let solvedCells = [];
+const imageSources = [
+    'images/love.jpg',
+    'images/idea.png',
+    'images/fun.jpg',
+    'images/energy.jpg',
+    'images/sleep.png',
+    'images/travel.png',
+    'images/yoga.jpg',
+    'images/learn.png',
+    'images/exercise.jpg'
+];
+const solvedTexts = [
+    'love',
+    'idea',
+    'fun',
+    'energy',
+    'sleep',
+    'travel',
+    'yoga',
+    'learn',
+    'exercise'
+];
+
+function closeModal() {
+    document.getElementById('imageModal').style.display = 'none';
+}
+
+function showImage(cell) {
+    const cellIndex = parseInt(cell.innerText) - 1;
+    if (solvedTexts.includes(cell.innerHTML)) return;
+
+    const modal = document.getElementById('imageModal');
+    const modalImage = document.getElementById('modalImage');
+    modalImage.src = imageSources[cellIndex];
+    modal.style.display = 'flex';
+
+    modalImage.ondblclick = () => solve(cell);
+}
+
+function solve(cell) {
+    const cellIndex = parseInt(cell.innerText) - 1;
+    cell.innerHTML = solvedTexts[cellIndex];
+    solvedCells.push(cell.innerText);
+    closeModal();
+    checkAllSolved();
+}
+
+function checkAllSolved() {
+    if (solvedCells.length === 9) {
+        document.getElementById('finalSolveButton').classList.remove('hidden');
+    }
+}
+
+function showFinalImage() {
+    document.querySelector('table').style.display = 'none';
+    document.getElementById('finalSolveButton').style.display = 'none';
+    document.getElementById('imageModal').style.display = 'none';
+
+    const lifestyleText = document.createElement('div');
+    lifestyleText.id = 'lifestyleText';
+    const text = 'LIFESTYLE';
+    const colors = ['#00008B', '#8B0000', '#006400', '#8B008B', '#FF8C00', '#8B4513', '#2F4F4F', '#4B0082', '#B22222'];
+
+    text.split('').forEach((char, i) => {
+        const span = document.createElement('span');
+        span.innerText = char;
+        span.style.color = colors[i % colors.length];
+        lifestyleText.appendChild(span);
+    });
+
+    document.body.appendChild(lifestyleText);
+}
+
+window.onclick = (event) => {
+    if (event.target == document.getElementById('imageModal')) {
+        closeModal();
+    }
+}
